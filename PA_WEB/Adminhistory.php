cc45c03947f7433a0792190c9ed41f51567cfb29
Aduasatu <alan992001@gmail.com>
2022-12-01 09:44:37 +0800

    Update

--- PA_WEB/Adminhistory.php
+++ PA_WEB/Adminhistory.php
@@ -12,8 +12,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin History</title>
-    <link rel="stylesheet" href="dataTables/datatables.min.css">
-
     <link rel="shortcut icon" type="image/jpg" href="img/Kucing.png"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
