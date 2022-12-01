06cfd0144be8a0196d7fe8e5c58409776dc4456c
Aduasatu <alan992001@gmail.com>
2022-12-01 09:54:19 +0800

    up

--- PA_WEB/Adminhistory.php
+++ PA_WEB/Adminhistory.php
@@ -120,12 +120,6 @@
 <?php endif;?>
     </div>
     </div>
-    <script src="dataTables/datatables.min.js"></script>
-    <script>
-    $(document).ready(function () {
-    $('#tabel').DataTable();
-    });
-    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
